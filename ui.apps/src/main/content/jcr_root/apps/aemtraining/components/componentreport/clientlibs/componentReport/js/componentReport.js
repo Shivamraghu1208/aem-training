72387edb2dfea22a0289f406689dceab15b4c09d
Shivam Raghuwanshi <shivam@gmail.com>
2024-05-30 17:41:01 +0530

    adding clientlibs

--- ui.apps/src/main/content/jcr_root/apps/aemtraining/components/componentreport/clientlibs/componentReport/js/componentReport.js
+++ ui.apps/src/main/content/jcr_root/apps/aemtraining/components/componentreport/clientlibs/componentReport/js/componentReport.js
@@ -1,28 +1,28 @@
- document.addEventListener("DOMContentLoaded",()=>{
- document.querySelector('.button').addEventListener('click', function() {
-     let dropdown = document.querySelector('.dropdown-value');
-     let value = dropdown.value;
-     console.log(value);
-          let apiUrl = "http://localhost:4502/content/aemtraining/Student/ss/jcr:content/root/container/componentreport.json?Path="+value;
+  document.addEventListener("DOMContentLoaded",()=>{
+  document.querySelector('.button').addEventListener('click', function() {
+      let dropdown = document.querySelector('.dropdown-value');
+      let value = dropdown.value;
 
-         $.ajax({
-          method: 'get',
-          url: apiUrl,
-          success: function(success){
-           $(".response-container").html('');
-          for(var i=0;i<success.length;i++)
-          {
-          console.log(success[i]);
-          $(".response-container").append('<div>'+success[i]+'</div>');
-          }
-            },
-          error: function(error){
-          console.log(error);
-          }
-         })
+      let componentResourcePath= $('.path-container').data("path")
 
-     });
+      let apiUrl = componentResourcePath+".json?Path="+value;
 
-  });
+            $.ajax({
+           method: 'get',
+           url: apiUrl,
+           success: function(success){
+            $(".response-container").html('');
+           for(var i=0;i<success.length;i++)
+           {
+              $(".response-container").append('<div>'+success[i]+'</div>');
+           }
+             },
+           error: function(error){
+            console.log(error);
+           }
+          })
 
 
+      });
+
+   });
