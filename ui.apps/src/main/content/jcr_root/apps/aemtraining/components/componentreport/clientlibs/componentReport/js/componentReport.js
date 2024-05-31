0dccc2be390af2933bb243e72a1244e3ed71eb71
Shivam Raghuwanshi <shivam@gmail.com>
2024-05-31 12:44:53 +0530

    adding updated js and html file

--- ui.apps/src/main/content/jcr_root/apps/aemtraining/components/componentreport/clientlibs/componentReport/js/componentReport.js
+++ ui.apps/src/main/content/jcr_root/apps/aemtraining/components/componentreport/clientlibs/componentReport/js/componentReport.js
@@ -1,28 +1,38 @@
-  document.addEventListener("DOMContentLoaded",()=>{
-  document.querySelector('.button').addEventListener('click', function() {
-      let dropdown = document.querySelector('.dropdown-value');
-      let value = dropdown.value;
-
-      let componentResourcePath= $('.path-container').data("path")
-
-      let apiUrl = componentResourcePath+".json?Path="+value;
-
-            $.ajax({
-           method: 'get',
-           url: apiUrl,
-           success: function(success){
-            $(".response-container").html('');
-           for(var i=0;i<success.length;i++)
-           {
-              $(".response-container").append('<div>'+success[i]+'</div>');
-           }
-             },
-           error: function(error){
-            console.log(error);
-           }
-          })
+ document.addEventListener("DOMContentLoaded",()=>{
 
 
-      });
+  var componentReport = document.querySelectorAll(".componentreport")
+  for(let i=0;i<componentReport.length;i++){
+  componentReport[i].querySelector('.button').addEventListener('click', function() {
+  console.log("button clicked");
+		let value = componentReport[i].querySelector('.dropdown-value').value;
+        if(value!=null)
+        {
+        let id= componentReport[i].querySelector(".response-container").getAttribute("id");
+		let componentResourcePath= componentReport[i].querySelector(".path-container").dataset.path;
+		let apiUrl = componentResourcePath+".json?Path="+value;
+        let currentIndex = componentReport[i];
+      console.log("apiUrl : "+apiUrl);
+		$.ajax({
+          method: 'get',
+          url: apiUrl,
+          success: function(success){
+		  currentIndex.querySelector(".response-container").innerHTML="";
+          for(var i=0;i<success.length;i++)
+          {
 
-   });
+            $('#'+id).append('<div>'+success[i]+'</div>');
+
+          }
+            },
+          error: function(error){
+           console.log(error);
+          }
+         })
+        }
+  });
+  }
+
+  });
+
+
