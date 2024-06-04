e86322300725db403352be7d4a17a7d54d04c0fc
Shivam Raghuwanshi <shivam@gmail.com>
2024-06-04 12:41:57 +0530

    updating HomePage Banner

--- core/src/main/java/com/adobe/aem/sample/site/core/models/HomePageBannerModel.java
+++ core/src/main/java/com/adobe/aem/sample/site/core/models/HomePageBannerModel.java
@@ -31,6 +31,7 @@ public class HomePageBannerModel {
     @ValueMapValue
     private String fileReference;
 
+
     @SlingObject
     private SlingHttpServletRequest request;
 
@@ -40,27 +41,35 @@ public class HomePageBannerModel {
     @SlingObject
     private Resource resource;
 
-    private String path;
+
 
     private boolean isPublishMode;
 
     @PostConstruct
     protected void init() {
         if (this.title == null) {
-            this.path = this.currentPage.getPath();
-            this.path = this.path.concat("/jcr:content");
+           String path = this.currentPage.getPath();
+              path = path.concat("/jcr:content");
             if (this.resource != null) {
                 ResourceResolver resourceResolver = this.resource.getResourceResolver();
-                Resource pageResource = resourceResolver.getResource(this.path);
+                Resource pageResource = resourceResolver.getResource(path);
                 if (pageResource != null) {
                     ValueMap valueMap = pageResource.getValueMap();
                     this.title = (String)valueMap.get("jcr:title", "");
                 }
             }
         }
+        if (fileReference==null) {
+            Resource image = resource.getChild("image");
+            if(image !=null) {
+                fileReference = image.getPath();
+            }
+        }
+
         WCMMode wcmMode = WCMMode.fromRequest((ServletRequest)this.request);
         if (wcmMode != null)
             this.isPublishMode = (wcmMode == WCMMode.DISABLED);
+
     }
 
     public String getButtonLabel() {
