427a0dd8ed9e1ade1c314e49893d6d437a850875
Shivam Raghuwanshi <shivam.raghuwanshi@aurx.co>
2024-07-08 11:31:03 +0530

    refactor code

--- core/src/main/java/com/adobe/aem/sample/site/core/servlets/GenerateKey.java
+++ core/src/main/java/com/adobe/aem/sample/site/core/servlets/GenerateKey.java
@@ -1,14 +1,14 @@
 package com.adobe.aem.sample.site.core.servlets;
 
 import com.adobe.aem.sample.site.core.services.TokenDetailService;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.SlingHttpServletResponse;
-import org.apache.sling.api.resource.LoginException;
 import org.apache.sling.api.resource.ModifiableValueMap;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
-import org.apache.sling.api.resource.ResourceResolverFactory;
 import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
 import org.json.JSONException;
@@ -21,7 +21,6 @@ import org.slf4j.LoggerFactory;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
@@ -36,68 +35,79 @@ public class GenerateKey extends SlingSafeMethodsServlet {
 
     @Reference
     private transient TokenDetailService tokenDetailService;
-    @Reference
-    private ResourceResolverFactory resourceResolverFactory;
+
     private Logger log = LoggerFactory.getLogger(ComponentReportServlet.class);
 
     @Override
     protected void doGet(SlingHttpServletRequest request, SlingHttpServletResponse response) throws ServletException, IOException {
         String responseObj = "";
+        JsonObject responseJson = new JsonObject();
         String name = request.getParameter("name");
         String email = request.getParameter("email");
         String path = tokenDetailService.getResourcePath();
         String token = request.getParameter("token");
 
         if (StringUtils.isNotBlank(name) && StringUtils.isNotBlank(email)) {
-            Map<String,Object> params=new HashMap<>();
-            params.put(ResourceResolverFactory.SUBSERVICE,"aem-training-content-reader");
-            ResourceResolver resourceResolver = null;
-            try {
-                resourceResolver = resourceResolverFactory.getServiceResourceResolver(params);
-            } catch (LoginException e) {
-                log.error("Login Exception {}", e.getMessage(),e);
-            }
-            if(resourceResolver!=null) {
-                Resource resource = resourceResolver.getResource(path);
-                if (resource != null) {
-                    String uniqueToken = UUID.randomUUID().toString();
-                    JSONObject jsonObject = new JSONObject();
-                    try {
-                        jsonObject.put("token", uniqueToken);
-                        jsonObject.put("email", email);
-                        jsonObject.put("name", name);
-                    } catch (JSONException e) {
-                        log.error("Json Exception {}", e.getMessage(),e);
-                    }
+            ResourceResolver resourceResolver = request.getResourceResolver();
+            Resource resource = resourceResolver.getResource(path);
+            if (resource != null) {
+                String uniqueToken = UUID.randomUUID().toString();
+                JSONObject jsonObject = new JSONObject();
+                try {
+                    jsonObject.put("token", uniqueToken);
+                    jsonObject.put("email", email);
+                    jsonObject.put("name", name);
                     ValueMap valueMap = resource.getValueMap();
-                    responseObj = checkForTokenInNode(email, resource, valueMap, resourceResolver, jsonObject, response, uniqueToken, name);
+                    responseJson = checkForTokenInNode(email, resource, valueMap, resourceResolver, jsonObject, response, uniqueToken, name);
                     resourceResolver.close();
+                } catch (JSONException e) {
+                    log.error("Json Exception {}",e.getMessage(),e);
                 }
+
             }
 
         } else if (StringUtils.isNotBlank(token)) {
+            JsonObject jsonObjectValidation =new JsonObject();
             Map<String, String> stringValueMap = tokenDetailService.getTokenDetails(token);
             if (stringValueMap != null && !stringValueMap.isEmpty()) {
                 String name1 = stringValueMap.get("name");
                 String email1 = stringValueMap.get("email");
-                responseObj = "Name:  " + name1 + " Email:  " + email1;
+
+                jsonObjectValidation.addProperty("name",name1);
+                jsonObjectValidation.addProperty("email",email1);
+                jsonObjectValidation.addProperty("status","Valid Token");
+                responseJson.add("result",jsonObjectValidation);
+
             } else {
-                responseObj = "Invalid Token";
+
+                jsonObjectValidation.addProperty("status","Error");
+                jsonObjectValidation.addProperty("message","Invalid Token");
+                responseJson.add("result", jsonObjectValidation);
             }
         }
-        response.getWriter().write(responseObj);
+
+        response.setContentLength(responseJson.toString().getBytes().length);
+        response.setContentType("application/json");
+        response.getOutputStream().write(responseJson.toString().getBytes());
     }
 
-    private synchronized String checkForTokenInNode(String email, Resource resource, ValueMap valueMap, ResourceResolver resourceResolver, JSONObject jsonObject, SlingHttpServletResponse response, String token, String name) throws IOException {
+    private synchronized JsonObject checkForTokenInNode(String email, Resource resource, ValueMap valueMap, ResourceResolver resourceResolver, JSONObject jsonObject, SlingHttpServletResponse response, String token, String name) throws IOException, JSONException {
+        JsonObject responseJsonObject = new JsonObject();
+        JsonObject tokenDetailsJson = new JsonObject();
         if (!valueMap.containsKey(email)) {
             resource.adaptTo(ModifiableValueMap.class).put(email, jsonObject.toString());
             resourceResolver.commit();
             tokenDetailService.storeTokenDetails(token, name, email);
-            return "New Token : " + token;
+            tokenDetailsJson.addProperty("new_Token",token);
+            tokenDetailsJson.addProperty("status","New Token");
+            responseJsonObject.add("result", tokenDetailsJson);
         } else {
-            String existingToken = valueMap.get(email, " ");
-            return "Already present : " + existingToken.toString();
-        }
 
+            String existingTokenJsonString = valueMap.get(email, " ");
+            tokenDetailsJson= JsonParser.parseString(existingTokenJsonString).getAsJsonObject();
+            tokenDetailsJson.addProperty("status","Already present");
+            responseJsonObject.add("result", tokenDetailsJson);
+        }
+        return responseJsonObject;
     }
 }
